92ffb1cc6fdb44da2ea9088f41e18d62ea3020bd
SagarTendulkar <140165649+SagarTendulkar@users.noreply.github.com>
2026-07-17 17:26:50 +0530

    feature: tanstack providers

--- src/hooks/useEmployees.tsx
+++ src/hooks/useEmployees.tsx
@@ -1,35 +1,41 @@
 import { getEmployees } from "@/services/employeeService";
-import type { Employee } from "@/types/employee";
-import { useEffect, useState } from "react";
+// import type { Employee } from "@/types/employee";
+import { useQuery } from "@tanstack/react-query";
+// import { useEffect, useState } from "react";
 
 const useEmployees = () => {
-    const [employees, setEmployees] = useState<Employee[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
+    // const [employees, setEmployees] = useState<Employee[]>([]);
+    // const [loading, setLoading] = useState(true);
+    // const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        const fetchEmployees = async () => {
-            setLoading(true);
-            setError(null);
-            try {
-                const res = await getEmployees();
-                setEmployees(res);
-            } catch (error: unknown) {
-                if (error instanceof Error) {
-                    setError(error.message);
-                }
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchEmployees();
-    }, []);
+    // useEffect(() => {
+    //     const fetchEmployees = async () => {
+    //         setLoading(true);
+    //         setError(null);
+    //         try {
+    //             const res = await getEmployees();
+    //             setEmployees(res);
+    //         } catch (error: unknown) {
+    //             if (error instanceof Error) {
+    //                 setError(error.message);
+    //             }
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     };
+    //     fetchEmployees();
+    // }, []);
+
+    const { data, isLoading, error } = useQuery({
+        queryKey: ["employees"],
+        queryFn: getEmployees,
+        staleTime: 1000 * 60 * 5, // 5 minutes
+    });
 
     return {
-        employees,
-        loading,
+        employees: data ?? [],
+        loading: isLoading,
         error,
-        setEmployees
     };
 };
 
